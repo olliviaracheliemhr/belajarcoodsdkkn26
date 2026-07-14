263caebd001033198dd0838853469ad4cffec839
olliviaracheliemhr <olliviaracheliemaharani@gmail.com>
2026-07-14 10:24:26 +0700

    Create soal.js

---
+++ soal.js
@@ -0,0 +1,123 @@
+const questions = [
+
+{
+title:"Latihan 1",
+question:"Tampilkan tulisan berikut.",
+text:"Hello World",
+code:"print( _____ )",
+options:[
+'"Hellow World"',
+'"Hello World"'
+],
+answer:1
+},
+
+{
+title:"Latihan 2",
+question:"Tampilkan tulisan berikut.",
+text:"Belajar Coding",
+code:"print( _____ )",
+options:[
+'"Belajar coding"',
+'"Belajar Coding"'
+],
+answer:1
+},
+
+{
+title:"Latihan 3",
+question:"Tampilkan tulisan berikut.",
+text:"Saya Suka Python",
+code:"print( _____ )",
+options:[
+'"Saya suka Python"',
+'"Saya Suka Python"'
+],
+answer:1
+},
+
+{
+title:"Latihan 4",
+question:"Tampilkan tulisan berikut.",
+text:"Universitas Islam Kadiri",
+code:"print( _____ )",
+options:[
+'"Universitas islam Kadiri"',
+'"Universitas Islam Kadiri"'
+],
+answer:1
+},
+
+{
+title:"Latihan 5",
+question:"Tampilkan tulisan berikut.",
+text:"Selamat Belajar",
+code:"print( _____ )",
+options:[
+'"Selamat belajar"',
+'"Selamat Belajar"'
+],
+answer:1
+},
+
+{
+title:"Latihan 6",
+question:"Tampilkan tulisan berikut.",
+text:"Aku Bisa Coding",
+code:"print( _____ )",
+options:[
+'"Aku Bisa Coding"',
+'"Aku Bisa Cooding"'
+],
+answer:0
+},
+
+{
+title:"Latihan 7",
+question:"Tampilkan tulisan berikut.",
+text:"Nama Saya Budi",
+code:"print( _____ )",
+options:[
+'"Nama saya Budi"',
+'"Nama Saya Budi"'
+],
+answer:1
+},
+
+{
+title:"Latihan 8",
+question:"Tampilkan tulisan berikut.",
+text:"Selamat Pagi",
+code:"print( _____ )",
+options:[
+'"Selamat Pagi"',
+'"Selamat pagi"'
+],
+answer:0
+},
+
+{
+title:"Latihan 9",
+question:"Tampilkan tulisan berikut.",
+text:"Saya Anak Indonesia",
+code:"print( _____ )",
+options:[
+'"Saya Anak Indonesia"',
+'"Saya anak Indonesia"'
+],
+answer:0
+},
+
+{
+title:"Latihan 10",
+question:"Tampilkan tulisan berikut.",
+text:"Coding Itu Menyenangkan",
+code:"print( _____ )",
+options:[
+'"Coding Itu Menyenangkan"',
+'"Coding itu Menyenangkan"'
+],
+answer:0
+}
+
+];
